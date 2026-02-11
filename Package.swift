89ac33d348ab68b05a161d540e0999391818f52f
Alan Wang <awang@anonyome.com>
2026-02-10 18:49:35 -0800

    0.14.1-anonyome-4 release

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 
 import PackageDescription
 
-let checksum = "e3244880d11f3078bf12d17591c8c163282f230d31d6d87e4352cb8a37b9f0bf"
-let version = "0.14.1-anonyome-3"
+let checksum = "56747c8f8160d15664d175b0dabe341d962a6f6396219c6b9d5b08887445e43e"
+let version = "0.14.1-anonyome-4"
 let url = "https://github.com/sudoplatform/matrix-rust-sdk-swift/releases/download/\(version)/MatrixRustSDK.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
