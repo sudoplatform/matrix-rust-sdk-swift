1ad365d58e5ce9b79bf9ac14c731cf575c38a11c
Glen Leeder <gleeder@anonyome.com>
2026-04-22 09:23:38 +1000

    0.14.1-anonyome-7 release

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 
 import PackageDescription
 
-let checksum = "55a434b9b0f01d9504257414f50093b972c793409e3d48b5bd9a6567e20da242"
-let version = "0.14.1-anonyome-6"
+let checksum = "d804e097052c3635a5dd35135c5955b67d80070d6275198cbca385f1daa553ec"
+let version = "0.14.1-anonyome-7"
 let url = "https://github.com/sudoplatform/matrix-rust-sdk-swift/releases/download/\(version)/MatrixRustSDK.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
